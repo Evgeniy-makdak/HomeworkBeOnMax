e8c05a6d96789393c669dd5b3023ec9a3de39041
Evgeniy-makdak <makdak23@mail.ru>
2026-03-03 16:22:42 +0300

    Добавил новый элемент меню с заголовком

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 const menuItems = document.querySelectorAll(".menu-item"),
   menu = document.querySelector(".menu");
 
-menu.insertBefore(menuItems[2], menuItems[1]);  
+menu.insertBefore(menuItems[2], menuItems[1]); 
+const menuItemFive = document.createElement("li");
+menuItemFive.textContent = "Пятый пункт";
+menuItemFive.classList.add("menu-item")
+menu.appendChild(menuItemFive);
