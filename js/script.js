125d4f53c36073a824f23d33a622e2af0759cfab
Evgeniy-makdak <makdak23@mail.ru>
2026-03-04 10:08:19 +0300

    Обновлена фоновая картинка

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 const menuItems = document.querySelectorAll(".menu-item"),
-  menu = document.querySelector(".menu");
-
+  menu = document.querySelector(".menu"),
+  body = document.querySelector("body");
 menu.insertBefore(menuItems[2], menuItems[1]); 
 const menuItemFive = document.createElement("li");
 menuItemFive.textContent = "Пятый пункт";
 menuItemFive.classList.add("menu-item")
 menu.appendChild(menuItemFive);
+body.style.background = "url(img/apple_true.jpg) center no-repeat";
