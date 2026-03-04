83818350390316d0af6ee9bc23eaf35e751428fd
Evgeniy-makdak <makdak23@mail.ru>
2026-03-04 10:59:27 +0300

    Удалён рекламный блок

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 const menuItems = document.querySelectorAll(".menu-item"),
   menu = document.querySelector(".menu"),
-  body = document.querySelector("body");
-menu.insertBefore(menuItems[2], menuItems[1]); 
+  body = document.querySelector("body"),
+  adv = document.querySelector(".adv"),
+  column = document.querySelectorAll(".column");
+
+menu.insertBefore(menuItems[2], menuItems[1]);
 const menuItemFive = document.createElement("li");
 menuItemFive.textContent = "Пятый пункт";
-menuItemFive.classList.add("menu-item")
+menuItemFive.classList.add("menu-item");
 menu.appendChild(menuItemFive);
 body.style.background = "url(img/apple_true.jpg) center no-repeat";
+column[1].removeChild(adv);
